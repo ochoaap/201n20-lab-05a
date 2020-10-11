3b1f01163acf8f5bf7acbf7ac1f07feb6119e5af
Anthony Ochoa <ochoaap@yahoo.com>
2020-10-10 17:22:13 -0700

    prob-4 completed

--- app.js
+++ app.js
@@ -90,12 +90,22 @@ Test this function by hand in the console to get it working, and when you think 
 var testArray = [2, 3, 4]; //eslint-disable-line
 
 function sumArray(sumArr) { //eslint-disable-line
-
+// var arraysum = sum(testArray[0],testArray[1]);
+// console.log(arraysum)
+// var arraysumtotal = sum(arraysum, testArray[2]);
+var total = 0;
+for(var i = 0; i < sumArr.length; i++){
+  total = sum(total, sumArr[i])[0];
 }
+var arrayanswer = sumArr + " was passed in as an array of numbers, and " + total + " is their sum.";
+console.log(arrayanswer);
+return [total, arrayanswer]
+}
+
 
 // Here is the test for sumArray(); uncomment it to run it
 
-// testSumArray(testArray);
+testSumArray(testArray);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
@@ -111,11 +121,11 @@ Test this function by hand in the console to get it working, and when you think 
 
 // Write your code here
 function multiplyArray(multArr) { //eslint-disable-line
-
+  
 }
 
 // Here is the test for multiplyArray(); uncomment it to run it
-// testMultiplyArray(testArray);
+ testMultiplyArray(testArray);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. 
 
